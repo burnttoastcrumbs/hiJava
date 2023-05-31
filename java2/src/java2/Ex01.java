1539d707110b24e6c258bb0ee2c36536c34fddf6
kimdoyeon <kimdoyeon@localhost>
2023-05-31 16:42:08 +0900

    ㅁㄴㅇㄹㅎ포ㅠㅓㅏㅣ

---
+++ java2/src/java2/Ex01.java
@@ -0,0 +1,25 @@
+package java2;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+if (score>100) {
+	System.out.println("구라치지마");
+} else if(score>=90) {
+		System.out.println("A");
+} else if (score>=80) {
+	System.out.println("B");
+} else if (score>=70) {
+	System.out.println("C");
+} else if (score>=60) {
+	System.out.println("D");
+} else {
+	System.out.println("F");
+}
+
+}
+
+}
